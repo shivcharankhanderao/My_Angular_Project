cde6469cf94a5b91a6e7d269167559c2d66de510
shivcharankhanderao <shivakhandera30@gmail.com>
2023-06-15 11:01:35 +0530

    component creation

--- My_First_Project/src/app/app.module.ts
+++ My_First_Project/src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
